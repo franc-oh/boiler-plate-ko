0f43bbfb2aec300210fcdd0b3d39d5896aee1447
france1988 <france1988@naver.com>
2020-06-11 11:52:58 +0900

    20200611_Commit_~LoginToken

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const bodyparser = require('body-parser');
 app.use(bodyparser.urlencoded({extended: true})); //application/x-www-form-urlencoded 분석
 app.use(bodyparser.json()); //application/json 분석
 
-// 3-1. Register Route 생성
+// 3-1. Register Route 구현
 app.post('/register', (req, res) => {
   
   // Client의 요청을 req.body(body-parser를 통해)에 담아 User 모델로 객체화. 
@@ -47,5 +47,45 @@ app.post('/register', (req, res) => {
         success: true
     })
   })
+})
+
+
+// 4. 로그인기능 구현
+const cookieParser = require('cookie-parser');  // 생성된 토큰을 쿠키에 저장하기 위함
+
+app.use(cookieParser());
+
+// 4-1. login Route 구현
+app.post('/login', (req, res) => {
+
+  // 요청 이메일을 DB에서 존재여부 확인
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    // 이메일이 존재한다면 요청 비밀번호 존재여부 및 맞는지 확인 __(체크 메서드는 해당 모델에서 구현)
+    user.comparePassword( req.body.password, (err, isMatch) => {
+      if(!isMatch)
+        return res.json({ loginSuccess: false, message: "비번 틀림!!" })
+
+      // 비밀번호까지 같다면 Token 생성 __(생성 메서드는 해당 모델에서 구현)
+      user.generateToken( (err, user) => {
+        if(err) return res.status(400).send(err);
+
+        // 생성된 토큰을 쿠키에 저장 __(x_auth 쿠키로 토큰 저장)
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({ loginSuccess: true, userId: user._id })
+      })
+      
+    })
+
+  })
+
+
 })
 
--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+// === [1] MongoDB Model & Schema 생성
 // 1. DB모듈 가져오기
 const mongoose = require('mongoose');
 
@@ -11,6 +12,10 @@ const userSchema = mongoose.Schema({
     email:{
         type: String,
         trim: true,
+    },
+    password: {
+        type: String,
+        minlength: 5
     },
     lastname: {
         type: String,
@@ -29,6 +34,65 @@ const userSchema = mongoose.Schema({
     }
 
 });
+
+    // === [2] 비밀설정 암호화하기
+    const bcrypt = require('bcrypt');
+    
+    // --- 1. salt를 생성할 때 필요한 saltRounds 생성 (리터럴=암호화할 자릿수)
+    const saltRounds = 10; 
+
+    // --- 2-1. 해당 스키마가 save되기 전 암호화되도록 구현 __(next()를 통해 다음처리로 이동.)
+    userSchema.pre('save', function( next ) {
+        var user = this;    // 순수 비번을 가져오기 위해 User스키마 변수 선언
+        
+        // --- 2-2. 비밀번호가 변경되었을 경우에만 암호화 로직 처리되도록 구현
+        if(user.isModified('password')) {
+
+            // --- 2-3. 암호화 처리를 위해 salt 생성
+            bcrypt.genSalt(saltRounds, function(err, salt) {
+                if(err) return next(err)
+
+                // --- 2-4. salt를 가지고 암호화 처리 __(user.password = 순수 비번)
+                bcrypt.hash(user.password, salt, function(err, hash) {
+                    if(err) return next(err)
+                    user.password = hash;   // 순수 비번을 암호화 비번으로 대치
+                    next()
+                })
+            })
+        } else {
+            next()
+        }
+
+    })
+
+
+    // === [3] 로그인 기능 구현
+    
+    // --- 1. 비밀번호 존재여부 확인하는 메소드 생성
+    userSchema.methods.comparePassword = function(planePassword, cb) {
+        // planePassword를 암호화하여 비교
+        bcrypt.compare(planePassword, this.password, function(err, isMatch) {
+            if(err) return cb(err);
+            cb(null, isMatch);
+        })
+
+    }
+
+    // --- 2. token생성하는 메소드 생성
+    const jwt = require('jsonwebtoken')
+
+    userSchema.methods.generateToken = function(cb) {
+        var user = this;
+
+        // jsonwebtoken을 이용해 토큰생성
+        var token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+        user.token = token;
+        user.save(function(err, user) {
+            if(err) return cb(err);
+            cb(null, user);
+        })
+    }
 
 // 3. 모델에 스키마 맵핑
 const User = mongoose.model('User', userSchema);
